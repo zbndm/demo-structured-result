f87d17abfaf3a2f1302b23add9241422e56381c0
Kyle Rego <kyle.rego@algolia.com>
2017-03-03 17:23:16 -0800

    replace structured data index name string with constant

--- app/js/script.js
+++ app/js/script.js
@@ -8,6 +8,7 @@
 const APPLICATION_ID = 'VC519DRAY3';
 const SEARCH_ONLY_API_KEY = '5c796d39dcd489e62b89b38dae03fbc4';
 const INDEX_NAME = 'altCorrecTest';
+const STRUCTURED_DATA_INDEX_NAME = 'altCorrecTest_logos';
 const PARAMS = {
   hitsPerPage: 10,
   index: INDEX_NAME,
@@ -17,7 +18,7 @@ const PARAMS = {
 const algolia = algoliasearch(APPLICATION_ID, SEARCH_ONLY_API_KEY);
 const algoliaHelper = algoliasearchHelper(algolia, INDEX_NAME, PARAMS);
 const structuredDataHelper = algoliaHelper.derive(searchParameters => {
-    const params = searchParameters.setIndex('altCorrecTest_logos')
+    const params = searchParameters.setIndex(STRUCTURED_DATA_INDEX_NAME)
     params.getRankingInfo = true
     return params
 });
